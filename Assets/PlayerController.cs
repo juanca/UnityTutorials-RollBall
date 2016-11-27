42b707ce133503a6e1519ac6d673d3bda05cf9d4
Juan Carlos Medina <juanca.med@gmail.com>
2016-11-27 14:20:07 -0800

    Pick up PickUp objects
    Aaaand more code formatting shenanigans :)

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,30 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour
-{
+public class PlayerController : MonoBehaviour {
 
+  private int pickUpCount;
   private Rigidbody rb;
 
   // Use this for initialization
-  void Start ()
-  {
+  void Start () {
+    pickUpCount = 0;
     rb = GetComponent<Rigidbody> ();
   }
 
   // Update is called once per frame
-  void Update ()
-  {
+  void Update () {
 
   }
 
   // FixedUpdate is called once per fixed frame
-  void FixedUpdate ()
-  {
+  void FixedUpdate () {
     float horizontal = Input.GetAxis ("Horizontal");
     float vertical = Input.GetAxis ("Vertical");
     Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);
 
     rb.AddForce (movement);
+  }
+
+  void OnTriggerEnter (Collider other) {
+    if (other.gameObject.CompareTag ("PickUp")) {
+      other.gameObject.SetActive (false);
+      pickUpCount += 1;
+    }
   }
 }

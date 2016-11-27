d34d8845829f72b7818be9988cf7d9df4a463a1a
Juan Carlos Medina <juanca.med@gmail.com>
2016-11-27 14:04:06 -0800

    Pick ups!

---
+++ Assets/PickUpController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Vector3 rotation = new Vector3 (15, 30, 45);
+    transform.Rotate (rotation * Time.deltaTime);
+	}
+}

435af94f9460c5d6771e8495c2ed73bc468e7c47
Juan Carlos Medina <juanca.med@gmail.com>
2016-11-27 14:11:27 -0800

    Messing with code formatting in MonoDevelop

--- Assets/PickUpController.cs
+++ Assets/PickUpController.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PickUpController : MonoBehaviour {
+public class PickUpController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+  // Use this for initialization
+  void Start ()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update ()
+  {
     Vector3 rotation = new Vector3 (15, 30, 45);
     transform.Rotate (rotation * Time.deltaTime);
-	}
+  }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,22 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
   private Rigidbody rb;
 
-	// Use this for initialization
-	void Start () {
+  // Use this for initialization
+  void Start ()
+  {
     rb = GetComponent<Rigidbody> ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  }
+
+  // Update is called once per frame
+  void Update ()
+  {
+
+  }
 
   // FixedUpdate is called once per fixed frame
-  void FixedUpdate () {
+  void FixedUpdate ()
+  {
     float horizontal = Input.GetAxis ("Horizontal");
     float vertical = Input.GetAxis ("Vertical");
     Vector3 movement = new Vector3 (horizontal, 0.0f, vertical);

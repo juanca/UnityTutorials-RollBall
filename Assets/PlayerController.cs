3c0068dfb68610abe4fa17283bfc5df69e61f9f1
Juan Carlos Medina <juanca.med@gmail.com>
2016-11-26 19:22:24 -0800

    Player (sphere) with centered terrain

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+  private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+    rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  // FixedUpdate is called once per fixed frame
+  void FixedUpdate () {
+    float horizontal = Input.GetAxis ("Horizontal");
+    float vertical = Input.GetAxis ("Vertical");
+    Vector3 movement = new Vector3 (vertical, 0.0f, horizontal);
+
+    rb.AddForce (movement);
+  }
+}
